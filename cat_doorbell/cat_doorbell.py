ed33cb639f48a69c9873d1c7948c05a72f21ce17
caccolillo <caccolillo@yahoo.com>
2023-06-24 21:19:37 +0100

    Added the first release of CNN image detector

--- cat_doorbell/cat_doorbell.py
+++ cat_doorbell/cat_doorbell.py
@@ -1,32 +1,40 @@
 
 # import the libraries
-import cv2
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications.resnet import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications import imagenet_utils
+from imutils.object_detection import non_max_suppression
 import numpy as np
+import argparse
 import imutils
+import time
+import cv2
+
 
 #sliding window function
 def sliding_window(image, step, ws):
-	# slide a window across the image
-	for y in range(0, image.shape[0] - ws[1], step):
-		for x in range(0, image.shape[1] - ws[0], step):
-			# yield the current window
-			yield (x, y, image[y:y + ws[1], x:x + ws[0]])
+    # slide a window across the image
+    for y in range(0, image.shape[0] - ws[1], step):
+        for x in range(0, image.shape[1] - ws[0], step):
+            # yield the current window
+            yield (x, y, image[y:y + ws[1], x:x + ws[0]])
 
 #create image pyramid
 def image_pyramid(image, scale=1.5, minSize=(224, 224)):
-	# yield the original image
-	yield image
-	# keep looping over the image pyramid
-	while True:
-		# compute the dimensions of the next image in the pyramid
-		w = int(image.shape[1] / scale)
-		image = imutils.resize(image, width=w)
-		# if the resized image does not meet the supplied minimum
-		# size, then stop constructing the pyramid
-		if image.shape[0] < minSize[1] or image.shape[1] < minSize[0]:
-			break
-		# yield the next image in the pyramid
-		yield image
+    # yield the original image
+    yield image
+    # keep looping over the image pyramid
+    while True:
+        # compute the dimensions of the next image in the pyramid
+        w = int(image.shape[1] / scale)
+        image = imutils.resize(image, width=w)
+        # if the resized image does not meet the supplied minimum
+        # size, then stop constructing the pyramid
+        if image.shape[0] < minSize[1] or image.shape[1] < minSize[0]:
+            break
+        # yield the next image in the pyramid
+        yield image
 
 #image pre-processing function  
 def preprocessing(frame):
@@ -86,11 +94,147 @@ def img_averaging(vid):
     average_frameint = average_frame .astype(np.uint8)
     return average_frameint
 
-    #return(cv2.add(frame,frame1,frame2,frame3)/4)
 
 
+
+
+def image_detection(orig,min_conf):
+
+  # resize the image such that it has the
+  # has the supplied width, and then grab its dimensions
+  orig = imutils.resize(orig, width=WIDTH)
+  (H, W) = orig.shape[:2]
+
+  # initialize the image pyramid
+  pyramid = image_pyramid(orig, scale=PYR_SCALE, minSize=ROI_SIZE)
+
+  # initialize two lists, one to hold the ROIs generated from the image
+  # pyramid and sliding window, and another list used to store the
+  # (x, y)-coordinates of where the ROI was in the original image
+  rois = []
+  locs = []
+  # time how long it takes to loop over the image pyramid layers and
+  # sliding window locations
+  start = time.time()
+
+  # loop over the image pyramid
+  for image in pyramid:
+      # determine the scale factor between the *original* image
+      # dimensions and the *current* layer of the pyramid
+      scale = W / float(image.shape[1])
+      # for each layer of the image pyramid, loop over the sliding
+      # window locations
+      for (x, y, roiOrig) in sliding_window(image, WIN_STEP, ROI_SIZE):
+          # scale the (x, y)-coordinates of the ROI with respect to the
+          # *original* image dimensions
+          x = int(x * scale)
+          y = int(y * scale)
+          w = int(ROI_SIZE[0] * scale)
+          h = int(ROI_SIZE[1] * scale)
+          # take the ROI and preprocess it so we can later classify
+          # the region using Keras/TensorFlow
+          roi = cv2.resize(roiOrig, INPUT_SIZE)
+          roi = img_to_array(roi)
+          roi = preprocess_input(roi)
+          # update our list of ROIs and associated coordinates
+          rois.append(roi)
+          locs.append((x, y, x + w, y + h))
+          # check to see if we are visualizing each of the sliding
+          # windows in the image pyramid
+          # clone the original image and then draw a bounding box
+          # surrounding the current region
+          clone = orig.copy()
+          cv2.rectangle(clone, (x, y), (x + w, y + h),(0, 255, 0), 2)
+	
+  # show how long it took to loop over the image pyramid layers and
+  # sliding window locations
+  end = time.time()
+  print("[INFO] looping over pyramid/windows took {:.5f} seconds".format(end - start))
+  # convert the ROIs to a NumPy array
+  rois = np.array(rois, dtype="float32")
+  # classify each of the proposal ROIs using ResNet and then show how
+  # long the classifications took
+  print("[INFO] classifying ROIs...")
+  start = time.time()
+  preds = model.predict(rois)
+  end = time.time()
+  print("[INFO] classifying ROIs took {:.5f} seconds".format(end - start))
+  # decode the predictions and initialize a dictionary which maps class
+  # labels (keys) to any ROIs associated with that label (values)
+  preds = imagenet_utils.decode_predictions(preds, top=1)
+  labels = {}
+  # loop over the predictions
+  for (i, p) in enumerate(preds):
+      # grab the prediction information for the current ROI
+      (imagenetID, label, prob) = p[0]
+      # filter out weak detections by ensuring the predicted probability
+      # is greater than the minimum probability
+      if prob >= min_conf:
+          # grab the bounding box associated with the prediction and
+          # convert the coordinates
+          box = locs[i]
+          # grab the list of predictions for the label and add the
+          # bounding box and probability to the list
+          L = labels.get(label, [])
+          L.append((box, prob))
+          labels[label] = L
+      # loop over the labels for each of detected objects in the image
+  for label in labels.keys():
+      # clone the original image so that we can draw on it
+      print("[INFO] showing results for '{}'".format(label))
+      clone = orig.copy()
+      # loop over all bounding boxes for the current label
+      for (box, prob) in labels[label]:
+          # draw the bounding box on the image
+          (startX, startY, endX, endY) = box
+          cv2.rectangle(clone, (startX, startY), (endX, endY),(0, 255, 0), 2)
+      # show the results *before* applying non-maxima suppression, then
+      # clone the image again so we can display the results *after*
+      # applying non-maxima suppression
+      clone = orig.copy()
+      # extract the bounding boxes and associated prediction
+      # probabilities, then apply non-maxima suppression
+      boxes = np.array([p[0] for p in labels[label]])
+      proba = np.array([p[1] for p in labels[label]])
+      boxes = non_max_suppression(boxes, proba)
+      # loop over all bounding boxes that were kept after applying
+      # non-maxima suppression
+      for (startX, startY, endX, endY) in boxes:
+          # draw the bounding box and label on the image
+          cv2.rectangle(clone, (startX, startY), (endX, endY),(0, 255, 0), 2)
+          y = startY - 10 if startY - 10 > 10 else startY + 10
+          cv2.putText(clone, label, (startX, y),cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
+  return clone
+
+
+
+
+
+
+
+
+
+
+
+#main function
 # define a video capture object
 vid = cv2.VideoCapture(0)
+
+
+# initialize variables used for the object detection procedure
+WIDTH = 800
+PYR_SCALE = 1.5
+WIN_STEP = 16
+ROI_SIZE = (300,300)
+INPUT_SIZE = (224, 224)
+
+min_conf = 0.85 #minimum confidence level
+# load our network weights from disk
+print("[INFO] loading network...")
+model = ResNet50(weights="imagenet", include_top=True)
+
+
+
 
 while(True):
       
@@ -112,7 +256,8 @@ while(True):
     cv2.imshow(' blurred  ', blur)
     # image detection with CNN tutorial
     # https://pyimagesearch.com/2020/06/22/turning-any-cnn-image-classifier-into-an-object-detector-with-keras-tensorflow-and-opencv/
-
+    detected = image_detection(avg,min_conf)
+    cv2.imshow(' detected  ', detected)
     #motion detection
     #https://www.life2coding.com/opencv-simple-motion-detection/
 
